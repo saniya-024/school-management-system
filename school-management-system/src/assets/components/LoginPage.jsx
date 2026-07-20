6cd5be499d6d32edd2a4d2d24c55da8b5cf73297
saniya-024 <saniyayadav0208@gmail.com>
2026-07-20 20:57:49 +0530

    add school

---
+++ school-management-system/src/assets/components/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='min-h screen flex items-center bg-gradient-to-br from-gray-100 to-blue-100 px-4">
+    <div
+    
+      
+    </div>
+  )
+  )
+}
+
+export default LoginPage;
